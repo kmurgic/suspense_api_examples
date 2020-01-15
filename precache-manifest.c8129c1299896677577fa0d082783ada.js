45a89de7654a8a4f2a6d83943b2a0a5f49877083
Keith Murgic <keith.murgic@brandpulse.io>
2020-01-14 19:02:48 -0600

    Updates

---
+++ precache-manifest.c8129c1299896677577fa0d082783ada.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b4d5a31f57fe8d4140017d8654a65a",
+    "url": "/suspense_api_examples/index.html"
+  },
+  {
+    "revision": "d2c30087ea98dac785eb",
+    "url": "/suspense_api_examples/static/css/main.ff0f4af9.chunk.css"
+  },
+  {
+    "revision": "bc3f4c51dbf61c99bbf9",
+    "url": "/suspense_api_examples/static/js/2.f6508b7e.chunk.js"
+  },
+  {
+    "revision": "cfd730580bfaa6e5787d01bd9463ea23",
+    "url": "/suspense_api_examples/static/js/2.f6508b7e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2c30087ea98dac785eb",
+    "url": "/suspense_api_examples/static/js/main.eb759940.chunk.js"
+  },
+  {
+    "revision": "2d62c4ba25e27398d62d",
+    "url": "/suspense_api_examples/static/js/runtime-main.cf4d1bcc.js"
+  },
+  {
+    "revision": "497b8e022cdb6287a4e5f87de1bfbae3",
+    "url": "/suspense_api_examples/static/media/Yoda.497b8e02.png"
+  },
+  {
+    "revision": "e9f93d513a39d2687a9bc849e74cb186",
+    "url": "/suspense_api_examples/static/media/hanSolo.e9f93d51.jpg"
+  },
+  {
+    "revision": "14e6dd5c8dbe0cd06c72524e964d040d",
+    "url": "/suspense_api_examples/static/media/leia.14e6dd5c.jpg"
+  },
+  {
+    "revision": "be0d9fd5a1c00a70c7cc41abd73709ff",
+    "url": "/suspense_api_examples/static/media/loadingSpinner.be0d9fd5.gif"
+  },
+  {
+    "revision": "7062f8ace037f344cad1d568ea72cbbe",
+    "url": "/suspense_api_examples/static/media/luke.7062f8ac.jpg"
+  },
+  {
+    "revision": "6576a96aafaa055efc993a573e0286f0",
+    "url": "/suspense_api_examples/static/media/obiwan.6576a96a.jpg"
+  },
+  {
+    "revision": "32fb73ae5960485d9edfd916252559a4",
+    "url": "/suspense_api_examples/static/media/palpatine.32fb73ae.jpeg"
+  },
+  {
+    "revision": "1dcacb0397f71a31511f25db5355c067",
+    "url": "/suspense_api_examples/static/media/vader.1dcacb03.png"
+  }
+]);
